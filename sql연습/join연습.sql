4e6af56de202347e248e926740a27f713e3ba25a
백재원 <bjw1622@gmail.com>
2023-08-30 15:53:37 +0900

    feat : sql 연

---
+++ sql연습/join연습.sql
@@ -0,0 +1,8 @@
+-- inner join
+
+-- 예제1: 현재, 근무하고 있는 직원 이름과 직책을 모두 출력
+select a.emp_no,a.first_name, b.title
+	from employees a, titles b 
+    where a.emp_no = b.emp_no
+    and b.to_date = '9999-01-01';
+    
